b006ef85cd518c5f46240a309bfcba35a254d1f0
Jay Sim <jay.sim@vshn.net>
2023-06-29 15:56:22 -0700

    Added example exercises

---
+++ bonus/coding exercise.py
@@ -0,0 +1,6 @@
+import random
+
+lower_bound = int(input("Enter a number: "))
+upper_bound = int(input("Enter a number: "))
+
+print(random.randint(lower_bound, upper_bound))
---
+++ bonus/import webbrowser.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+user_term = input("Enter a search term: ")
+
+webbrowser.open("https://www.google.com/search?client=safari&rls=en&q=" + user_term)
