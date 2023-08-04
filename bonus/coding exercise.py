aa0d54f965fe38321c56a4db5bc20e042e557073
Jay Sim <jay.sim@vshn.net>
2023-08-04 11:13:18 -0700

    clean up some files

--- bonus/coding exercise.py
+++
@@ -1,6 +0,0 @@
-import random
-
-lower_bound = int(input("Enter a number: "))
-upper_bound = int(input("Enter a number: "))
-
-print(random.randint(lower_bound, upper_bound))
--- bonus/import webbrowser.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-user_term = input("Enter a search term: ")
-
-webbrowser.open("https://www.google.com/search?client=safari&rls=en&q=" + user_term)
